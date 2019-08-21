940a8ee6568cfaaf7c861fcd09f6f3f879e9e386
Mike Huang <wellwind@mail2000.com.tw>
2019-08-21 13:40:15 +0800

    練習 ngIf 和 ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = '聯合大學';
   imageWidth = 100;
+  display = true;
+
+  list = ['A', 'B', 'C', 'D'];
 
   imageResize(event: MouseEvent) {
+
+    this.display = !this.display;
+
+    // this.list.push('E');
+    this.list = [...this.list, 'E'];
+
+    // let arr = [];
+    // for(let i = 0;i<=this.list.length; +=i){
+    //   arr.push(this.list[i]);
+    // }
+    // this.list = arr;
+
+    console.log(this.list);
+
     console.log(event);
     if (event.ctrlKey) {
       this.imageWidth += 50;
