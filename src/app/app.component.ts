9bcb4c3f0bb0608c0e85c45115c7c8285fdec50d
Mike Huang <wellwind@mail2000.com.tw>
2019-08-21 11:23:08 +0800

    練習四種 data binding 的方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng20190821';
+  title = '聯合大學';
+  imageWidth = 100;
+
+  imageResize(event: MouseEvent) {
+    console.log(event);
+    if (event.ctrlKey) {
+      this.imageWidth += 50;
+    }
+  }
+
+  changeTitle(event) {
+    console.log(event.target.value);
+    this.title = event.target.value;
+  }
 }
